335c45b3bdce0854940cc8bb457adfbfbf386154
Rojae Martin <rojaedmartin@gmail.com>
2022-05-12 13:21:51 -0500

    removed summaries

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PortfoliosModule } from './portfolios/portfolios.module';
 import { TradingDaysModule } from './trading-days/trading-days.module';
 import { IndicesModule } from './indices/indices.module';
-import { SummariesModule } from './summaries/summaries.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule, PortfoliosModule, TradingDaysModule, IndicesModule, SummariesModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    PortfoliosModule,
+    TradingDaysModule,
+    IndicesModule,
+  ],
 })
 export class AppModule {}
